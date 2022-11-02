b472cae4fda39afcdf9179457fd5cf4ee3b23658
B-Perederei <72132841+B-Perederei@users.noreply.github.com>
2022-11-02 14:52:47 +0200

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,9 +1,9 @@
 SELECT plat_name, SUM(copies_sold) as copies_sold 
-FROM (gameplatformregionsales INNER JOIN platform  ON  gameplatformregionsales.plat_id = platform.plat_id)
+FROM (gameplatformregionsales INNER JOIN platform ON gameplatformregionsales.plat_id = platform.plat_id)
 GROUP BY platform.plat_id;
 
 SELECT plat_name, COUNT(game_id) as releases
-FROM (gameplatform INNER JOIN platform  ON gameplatform.plat_id = platform.plat_id)
+FROM (gameplatform INNER JOIN platform ON gameplatform.plat_id = platform.plat_id)
 GROUP BY platform.plat_id;
 
 SELECT release_year, COUNT(game_id) as releases
